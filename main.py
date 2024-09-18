cff70dcc4ff7e1355d069232845749f91d9408c0
Anthony Suárez <neolight1010@gmail.com>
2024-09-18 16:07:54 -0500

    Create schedulers

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
+from multiprocessing import Queue
+import threading
 import time
+import logging
 
 from workers.wiki_worker import WikiWorker
-from workers.yahoo_finane_worker import YahooFinanceWorker
+from workers.yahoo_finane_worker import DONE, YahooFinancePriceScheduler
+
 
 def main() -> None:
+    symbol_queue: Queue[str] = Queue()
     start_time = time.time()
     
     wiki = WikiWorker()
+    yahoo_scheduler_threads: list[threading.Thread] = []
 
-    current_workers: list[YahooFinanceWorker] = []
+    num_yahoo_schedulers = 10
+    yahoo_scheduler_threads: list[threading.Thread] = [YahooFinancePriceScheduler(symbol_queue) for _ in range(num_yahoo_schedulers)]
+    
     for symbol in wiki.get_sp_500_companies():
-        yahoo = YahooFinanceWorker(symbol)
-        current_workers.append(yahoo)
+        logging.debug(f"inserting symbol: {symbol}")
+        symbol_queue.put(symbol)
 
-    for worker in current_workers:
-        worker.join()
+    for _ in yahoo_scheduler_threads:
+        symbol_queue.put(DONE)
 
-    print("Execution took:", time.time() - start_time)
+    for thread in yahoo_scheduler_threads:
+        thread.join()
+
+    logging.info("Execution took:", time.time() - start_time)
 
 
 if __name__ == "__main__":
--- workers/yahoo_finane_worker.py
+++ workers/yahoo_finane_worker.py
@@ -1,9 +1,31 @@
+from __future__ import annotations
+
+import logging
+from multiprocessing import Queue
 import threading
 import bs4
 import random
 import requests
 import time
 
+DONE = "DONE"
+
+class YahooFinancePriceScheduler(threading.Thread):
+    def __init__(self, input_queue: Queue[str]):
+        super().__init__()
+        self._input_queue = input_queue
+        self.start()
+
+    def run(self):
+        while True:
+            symbol = self._input_queue.get()
+            if symbol == DONE:
+                break
+
+            yahoo = YahooFinanceWorker(symbol)
+            price = yahoo.get_price()
+            print(price)
+            time.sleep(20 * random.random())
 
 class YahooFinanceWorker(threading.Thread):
     def __init__(self, symbol: str):
@@ -15,6 +37,7 @@ class YahooFinanceWorker(threading.Thread):
         self.start()
 
     def get_price(self) -> float | None:
+        logging.info(f"getting price for symbol: {self._symbol}")
         response = requests.get(self._url)
 
         if response.status_code != 200:
@@ -23,9 +46,8 @@ class YahooFinanceWorker(threading.Thread):
         soup = bs4.BeautifulSoup(response.text, "html.parser")
 
         price_element = soup.find('fin-streamer', class_="livePrice")
-        return float(price_element.text) if price_element else None
+        return float(price_element.text.replace(",", "")) if price_element else None
 
     def run(self):
-        time.sleep(20 * random.random())
         print(self.get_price())
 
