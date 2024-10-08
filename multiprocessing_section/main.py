a330792d2450f4ef6beb4db2865fcd11d9846efe
Anthony Suárez <neolight1010@gmail.com>
2024-10-07 20:11:06 -0500

    Use multiprocessing queues

--- multiprocessing_section/main.py
+++ multiprocessing_section/main.py
@@ -1,12 +1,27 @@
 import multiprocessing
+import queue
 import time
+from typing import Literal
 
-def main() -> None:
-    num_threads = 4
+_DONE_T = Literal["DONE"]
+_DONE: _DONE_T = "DONE"
+
+
+def _main() -> None:
+    num_processes = 4
     comparison_list = [1, 2, 3]
+    queue_: multiprocessing.Queue[tuple[int, int, int] | _DONE_T] = (
+        multiprocessing.Queue()
+    )
 
     start_time = time.time()
-    processes = [multiprocessing.Process(target=check_value_in_list, args=(comparison_list,)) for _ in range(num_threads)]
+    processes = [
+        multiprocessing.Process(
+            target=_check_value_in_list,
+            args=(comparison_list, i, num_processes, queue_),
+        )
+        for i in range(num_processes)
+    ]
 
     for process in processes:
         process.start()
@@ -14,13 +29,31 @@ def main() -> None:
     for process in processes:
         process.join()
 
+    queue_.put(_DONE)
+
+    while (v := queue_.get()) != _DONE:
+        lower, upper, number_of_hits = v
+        print(
+            f"Between {lower} and {upper}, we have {number_of_hits} values in the list."
+        )
+
     print("everything took:", time.time() - start_time, "seconds")
 
-def check_value_in_list(x: list[float]) -> None:
-    for i in range(10**8):
-        if i in x:
-            print(f"{i} is in {x}")
+
+def _check_value_in_list(
+    x: list[float],
+    i: int,
+    number_of_processes: int,
+    queue: queue.Queue[tuple[int, int, int]],
+) -> None:
+    max_number_to_check_to = 10**8
+    lower = int(i * max_number_to_check_to / number_of_processes)
+    upper = int((i + 1) * max_number_to_check_to / number_of_processes)
+
+    hits = [n for n in range(lower, upper) if (n in x)]
+
+    queue.put((lower, upper, len(hits)))
 
 
 if __name__ == "__main__":
-    main()
+    _main()
